758d4724bd2e76208cccc1a7c9454dbf2a7d945c
Harryson <77166765+harrysonrbs@users.noreply.github.com>
2021-06-08 19:12:48 -0300

    Update data_input.py

--- data_input.py
+++ data_input.py
@@ -1,5 +1,12 @@
 """
                                                           DATA INPUT
-                                                          
+
+A very common task in a program or script is simply capturing data typed by the keyboard by a user, this data will then be processed for a particular task.
+
+In Python, to capture data typed on the keyboard by a user, we can employ the function input() which will activate the typing mode in the console, this way the 
+input() function will store a typed data type to a variable, for example. It is important to remember that we can pass a text (string) as a parameter to the input() 
+function, requesting more specific data from the user.
+
+
                                                
 """
